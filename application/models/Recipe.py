0c865fd092bf3a2bfdad7d6c06db41981c22a3c8
SamM71 <sammocock@gmail.com>
2023-09-25 12:36:48 +0100

    add recipe model

---
+++ application/models/Recipe.py
@@ -0,0 +1,26 @@
+from application import app, db
+
+app.app_context().push()
+
+class Recipe(db.Model):
+  __tablename__ = "recipes"
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  name = db.Column(db.String(100), nullable=False)
+  culture = db.Column(db.String(100), nullable=False)
+  description = db.Column(db.String(5000), nullable=False)
+  img_url = db.Column(db.String(100))
+
+  def __init__(self, user_id, name, culture, description, img_url):
+    self.user_id = user_id
+    self.name = name
+    self.culture = culture
+    self.description = description
+    self.img_url = img_url
+
+  def __repr__(self):
+    return f"Recipe(id: {self.id}, user_id: {self.user_id}, name: {self.name}, img_url: {self.img_url})"
+  
+  @property
+  def json(self):
+    return {"id": self.id, "user_id": self.user_id, "name": self.name, "description": self.description, "img_url": self.img_url}
