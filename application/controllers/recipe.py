68782946a749b926918a714148a4d7aed5944845
SamM71 <sammocock@gmail.com>
2023-09-25 15:12:44 +0100

    recipe controller

---
+++ application/controllers/recipe.py
@@ -0,0 +1,61 @@
+from ..models.Recipe import Recipe
+from werkzeug import exceptions
+from flask import jsonify, request
+from .. import db
+
+def index():
+  try:
+    recipes = Recipe.query.all()
+    data = [r.json for r in recipes]
+    print(data)
+    return jsonify({"recipes": data})
+  except:
+    raise exceptions.NotFound("No recipes found.")
+  
+def show(id):
+  try:
+    recipe = Recipe.query.filter_by(id=id).first()
+    return jsonify({"recipe": recipe.json}), 200
+  except:
+    raise exceptions.NotFound("Recipe not found.")
+  
+def show_by_name(name):
+  try:
+    recipe = Recipe.query.filter_by(name=name).first()
+    return jsonify({"recipe": recipe.json}), 200
+  except:
+    raise exceptions.NotFound("Recipe not found.")
+
+
+def create():
+  try:
+    user_id, name, culture, description, img_url = request.json.values()
+    new_recipe = Recipe(user_id, name, culture, description, img_url)
+    db.session.add(new_recipe)
+    db.session.commit()
+    return jsonify({"new_recipe": new_recipe.json}), 201
+  except:
+    raise exceptions.BadRequest(f"Unable to create new recipe with data provided.")
+  
+def update(id):
+  try:
+    data = request.json
+    recipe = Recipe.query.filter_by(id=id).first()
+
+    for (attr, val) in data.items():
+      if hasattr(recipe, attr):
+        setattr(recipe, attr, val)
+
+    db.session.commit()
+    return jsonify({"data": recipe.json})
+  except:
+    raise exceptions.BadRequest(f"Unable to update recipe with data provided.")
+  
+def delete(id):
+  try:
+    recipe = Recipe.query.filter_by(id=id).first()
+    db.session.delete(recipe)
+    db.session.commit()
+    return f"Recipe deleted"
+  except:
+    raise exceptions.InternalServerError(f"Unable to delete recipe.")
