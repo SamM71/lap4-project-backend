333efb9e17ef880d47360ff526c90cbb1bd579ba
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-26 14:38:25 +0100

    fix typo

--- seed.py
+++ seed.py
@@ -7,6 +7,7 @@ from application.models.Message import Message
 from application.models.Token import Token
 
 
+
 db.drop_all()
 db.create_all()
 
@@ -21,7 +22,13 @@ entry8 = Dialogue(username="Charlie", receiver="Sam", title="charlie-sam")
 entry9 = Message(username="Charlie", text="Hello, I am a message")
 entry10 = Message(username="Rubina", text="Hello")
 entry11 = Token(user_id=1, token='hbkdhcvhcsvcgsvchsvchlsl')
-db.session.add_all([entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8, entry9, entry10, entry11])
+entry12 = Dialogue(username="Charlie", receiver="sam1", title="charlie-sam1")
+entry13 = Dialogue(username="Charlie", receiver="rubina1", title="charlie-rubina1")
+entry14 = Dialogue(username="Charlie", receiver="charlie1", title="charlie-charlie1")
+entry15 = Dialogue(username="Charlie", receiver="ranti1", title="charlie-ranti1")
+entry16 = Dialogue(username="Charlie", receiver="hasan1", title="charlie-hasan1")
+db.session.add_all([entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8, entry9, entry10, entry11, entry12,
+                    entry13, entry14, entry15, entry16])
 db.session.commit()
 
 
