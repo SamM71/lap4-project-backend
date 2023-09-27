cc274c9015b51845f20006aca850f4e30f57dc14
SamM71 <sammocock@gmail.com>
2023-09-27 09:40:22 +0100

    add timestamp to Post model

--- application/models/Post.py
+++ application/models/Post.py
@@ -1,4 +1,5 @@
 from application import app, db
+import datetime
 
 app.app_context().push()
 
@@ -11,6 +12,7 @@ class Post(db.Model):
     description = db.Column(db.String(200), nullable=False)
     story = db.Column(db.String(100), nullable=False)
     img_url = db.Column(db.String(100))
+    time_posted = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __init__(self, user_id, recipe_id, description, story, img_url):
         self.user_id = user_id
@@ -25,4 +27,4 @@ class Post(db.Model):
     @property
     def json(self):
         return {"id": self.id, "user_id": self.user_id, "recipe_id": self.recipe_id,
-                "description": self.description, "story": self.story, "img_url": self.img_url}
+                "description": self.description, "story": self.story, "img_url": self.img_url, "time_posted": self.time_posted}
